6c625a0d014a65115887e981ba93bcf1693a8bc0
Forwamba <elishawamba61@gmail.com>
2025-05-22 10:12:08 +0100

    push

---
+++ Untitled1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+float celcius, fahrenheit;
+printf("enter temperature in celcuis");
+scanf("%f,& celcius");
+fahrenheit=(celcius*9.0/5.0)+32;
+printf("the temperature in fahrenheit is %.2f",fahrenheit);
+return0;
+}
